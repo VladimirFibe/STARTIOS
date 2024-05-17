e5a64ed393c54973fa94e5028414ebfa8a92bd52
Vladimir Fibe <motiw@icloud.com>
2024-05-17 23:58:59 +0500

    ui ready

---
+++ Calculator/Calculator/CalculatorButton.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+enum CalculatorButton: String {
+    case zero = "0"
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case point = "."
+    case equals = "="
+    case plus = "+"
+    case minus = "-"
+    case multiply = "×"
+    case divide = "÷"
+    case ac = "AC"
+    case plusMinus = "±"
+    case perecent = "%"
+
+    var title: String {
+        rawValue
+    }
+
+    var backgroundColor: UIColor {
+        switch self {
+        case .ac, .plusMinus, .perecent: return .systemGray
+        case .plus, .equals, .minus, .multiply, .divide: return .systemOrange
+        default: return .systemFill
+        }
+    }
+
+    var foregroundColor: UIColor {
+        switch self {
+        case .ac, .plusMinus, .perecent: return .systemBackground
+        default: return .label
+        }
+    }
+
+    var isDigit: Bool {
+        switch self {
+        case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .point :
+            return true
+        default:
+            return false
+        }
+    }
+}
--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class ViewController: UIViewController {
     private let stackView = UIStackView()
+    private let displayLabel = UILabel()
+    private var buttonZero: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         setupStackView()
@@ -9,14 +11,79 @@ class ViewController: UIViewController {
 
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        stackView.frame = view.bounds
+        stackView.frame = view.layoutMarginsGuide.layoutFrame
+
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        buttonZero.configuration?.contentInsets = .init(top: 0, leading: 0, bottom: 0, trailing: buttonZero.frame.height + 4)
+    }
+    
     private func setupStackView() {
         view.addSubview(stackView)
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.addArrangedSubview(UIView())
+        setupDisplayLabel()
+        stackView.addArrangedSubview(makeStack(with: makeViews(with: ["AC", "±", "%", "÷"])))
+        stackView.addArrangedSubview(makeStack(with: makeViews(with: ["7", "8", "9", "×"])))
+        stackView.addArrangedSubview(makeStack(with: makeViews(with: ["4", "5", "6", "-"])))
+        stackView.addArrangedSubview(makeStack(with: makeViews(with: ["1", "2", "3", "+"])))
+        stackView.addArrangedSubview(makeStack(with: [makeButton(with: "0"), makeStack(with: makeViews(with: [".", "="]))]))
     }
+
+    private func makeViews(with titles: [String]) -> [UIView] {
+        titles.map { makeButton(with: $0)}
+    }
+
+    private func makeStack(with views: [UIView]) -> UIStackView {
+        let stack = UIStackView(arrangedSubviews: views)
+        stack.spacing = 8
+        stack.distribution = .fillEqually
+        stack.alignment = .fill
+        return stack
+    }
+
+    private func makeButton(with text: String) -> UIButton {
+        let button = UIButton(type: .system)
+        var config = UIButton.Configuration.filled()
+        var attText = AttributedString(text)
+        attText.font = .systemFont(ofSize: 40, weight: .semibold)
+        config.attributedTitle = attText
+        config.baseBackgroundColor = .systemFill
+        config.baseForegroundColor = .label
+        config.cornerStyle = .capsule
+        button.configuration = config
+        if text != "0" {
+            button.widthAnchor.constraint(equalTo: button.heightAnchor).isActive = true
+        } else {
+            buttonZero = button
+        }
+        button.addAction(UIAction(handler: { _ in
+            self.touchDigit(text)
+        }), for: .primaryActionTriggered)
+        return button
+    }
+
+    private func setupDisplayLabel() {
+        stackView.addArrangedSubview(displayLabel)
+        displayLabel.text = "0"
+        displayLabel.textAlignment = .right
+        displayLabel.font = .systemFont(ofSize: 80)
+    }
+
+    private func touchDigit(_ text: String) {
+        print(text)
+    }
+
+    private func performOperation(_ text: String) {
+        print("Operation: ", text)
+    }
+
 }
 
+@available(iOS 17.0, *)
 #Preview {
     ViewController()
 }
