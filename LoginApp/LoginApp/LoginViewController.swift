f17f2b9612582a71b336876a41df608f7175ab00
Vladimir Fibe <motiw@icloud.com>
2024-05-19 19:57:25 +0500

    State and Binding

--- LoginApp/LoginApp/LoginViewController.swift
+++ LoginApp/LoginApp/LoginViewController.swift
@@ -2,13 +2,16 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let controller = segue.destination as? WelcomeViewController else { return }
-        controller.view.backgroundColor = .red
+        controller.name = emailTextField.text ?? ""
     }
 
     @IBAction func unwind(for segue: UIStoryboardSegue) {
-        view.backgroundColor = .green
+        emailTextField.text = ""
+        passwordTextField.text = ""
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
--- LoginApp/LoginApp/WelcomeViewController.swift
+++ LoginApp/LoginApp/WelcomeViewController.swift
@@ -1,5 +1,13 @@
 import UIKit
 
 final class WelcomeViewController: UIViewController {
+    var name = ""
+    @IBOutlet weak var welcomeLabel: UILabel!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if !name.isEmpty {
+            welcomeLabel.text = "Welcome, \(name)!"
+        }
+    }
 }
